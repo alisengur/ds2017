909c4154fdc61b2866314bbb31412ecd4760f7cf
caki <cakiaccounts@gmail.com>
2017-09-25 18:21:39 +0300

    25.09.2017 teorik ders

---
+++ ders/H2myqueue/H2myqueue/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace H2myqueue
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			MyQueue<int> queue = new MyQueue<int> ();
+			queue.enqueue (5);
+			queue.enqueue (7);
+			queue.enqueue (4);
+			Console.WriteLine ("Eleman sayısı:{0}",queue.Count);
+			queue.dequeue ();
+			queue.dequeue ();
+			queue.dequeue ();
+			Console.WriteLine ("Eleman sayısı:{0}",queue.Count);
+			queue.print ();
+			queue.enqueue (6);
+			Console.WriteLine ("Eleman sayısı:{0}",queue.Count);
+			queue.enqueue (5);
+			queue.enqueue (7);
+			queue.enqueue (4);
+			queue.print ();
+		}
+	}
+}
---
+++ ders/H2mystack/H2mystack/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace H2mystack
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			MyStack<int> stack = new MyStack<int> ();
+			stack.push (5);
+			stack.push (2);
+			stack.push (7);
+			stack.pop ();
+			stack.push (6);
+			stack.push (9);
+			stack.push (4);
+			stack.pop ();
+			stack.pop ();
+			stack.push (12);
+			stack.print ();
+			//while (true)
+			//	stack.pop ();
+			MyStack<string> stack2=new MyStack<string>();
+			stack2.push ("Şevket");
+			stack2.push ("Yuşa");
+			stack2.push("Lassine");
+			while(!stack2.isEmpty())
+				Console.WriteLine (stack2.pop());
+		}
+	}
+}
