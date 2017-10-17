7a17b61d84a5dbe4ea5c149585bc442b1346c4fc
caki <cakiaccounts@gmail.com>
2017-10-17 22:40:43 +0300

    17.10.2017 lab.

---
+++ lab/VY17102017/ConsoleApplication1/PostTree.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class PostTree
+    {
+        Node<char> root;
+        Stack<char> stk;
+
+        public PostTree(string postfix)
+        {
+            stk = new Stack<char>();
+            for (int i = 0; i < postfix.Length; i++)
+            {
+                stk.Push(postfix[i]);
+            }
+            root = new Node<char>(stk.Pop());
+            add(root);
+        }
+
+        void add(Node<char> node)
+        {
+            node.Right = new Node<char>(stk.Pop());
+            if (isOperator(node.Right.val))
+                add(node.Right);
+            node.Left = new Node<char>(stk.Pop());
+            if (isOperator(node.Left.val))
+                add(node.Left);
+        }
+
+        bool isOperator(char val)
+        {
+            return val == '+' || val == '-' || val == '*' || val == '/';
+        }
+
+        public void postOrder()
+        {
+            postOrderRecursive(root);
+        }
+
+        void postOrderRecursive(Node<char> node)
+        {
+            if (node == null)
+                return;
+            postOrderRecursive(node.Left);
+            postOrderRecursive(node.Right);
+            Console.WriteLine(node.val);
+        }
+    }
+}
---
+++ lab/VY17102017/ConsoleApplication1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //BinaryTree<int> bt = new BinaryTree<int>();
+            //bt.add(5);
+            //bt.add(3);
+            //bt.add(8);
+            //bt.add(1);
+            //bt.add(4);
+            //bt.add(7);
+            //bt.add(10);
+
+            ////bt.delete(1);
+            ////bt.delete(3);
+            ////bt.delete(8);
+            ////bt.delete(5);
+
+            //bt.postOrder();
+
+            PostTree pt = new PostTree("35+47**5/147-+*");
+
+            pt.postOrder();
+        }
+    }
+}
