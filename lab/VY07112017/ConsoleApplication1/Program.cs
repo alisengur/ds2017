774c94c8e6693441d8c5915ae72f647614bb9b2a
caki <cakiaccounts@gmail.com>
2017-11-10 00:12:24 +0300

    07.10.2017 lab.

---
+++ lab/VY07112017/ConsoleApplication1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SeperateChaining sc = new SeperateChaining();
+
+            sc.add(13);
+            sc.add(65);
+            sc.add(54);
+            sc.add(1);
+            sc.add(6);
+            sc.add(42);
+            sc.add(18);
+            sc.add(63);
+            sc.add(7);
+            sc.add(2);
+            sc.add(43);
+            sc.add(35);
+            sc.add(25);
+            sc.add(5);
+            sc.add(16);
+
+            sc.display();
+        }
+    }
+}
---
+++ lab/VY07112017/ConsoleApplication1/SeperateChaining.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class SeperateChaining
+    {
+        LinkedList<int>[] array = new LinkedList<int>[10];
+
+        public SeperateChaining()
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = new LinkedList<int>();
+            }
+        }
+
+        int Hash(int x)
+        {
+            return x % 10;
+        }
+
+        public void add(int val)
+        {
+            array[Hash(val)].AddFirst(val);
+        }
+
+        public void delete(int val)
+        {
+            array[Hash(val)].Remove(val);
+        }
+
+        public void display()
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write("("+i+")");
+                LinkedListNode<int> iterator = array[i].First;
+                while (iterator != null)
+                {
+                    Console.Write("->"+iterator.Value);
+                    iterator = iterator.Next;
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
